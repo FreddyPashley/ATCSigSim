e66fd2192feb023d2e53be0affcac46f6f3e528d
Freddy <fredpashley1@gmail.com>
2025-11-21 20:43:19 +0000

    Login system

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,44 @@
 import flask
 import os
+import json
 
-os.chdir("./mysite/")
+def getJSON(path):
+    with open(path) as f:
+        return json.load(f)
+    
+def writeJSON(path, content):
+    with open(path, "w") as f:
+        json.dump(content, f, skipkeys=True, indent=4)
+
+def logged_in():
+    return "logged_in" in flask.session and flask.session["logged_in"] == True and "user" in flask.session
+
+# os.chdir("./mysite/")
 app = flask.Flask(__name__)
+app.secret_key = "123"
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return flask.render_template("index.html")
+    warning = ""
+    if flask.request.method == "POST":
+        name = flask.request.form.get("name")
+        psw = flask.request.form.get("psw")
+        if name is not None and name in getJSON("data/users.json"):
+            flask.session.clear()
+            flask.session["logged_in"] = True
+            flask.session["user"] = name
+        else:
+            warning = f"Name '{name}' not in list"
+    return flask.render_template("index.html", warning=warning)
+
+@app.route("/logout")
+def logout():
+    flask.session.clear()
+    return flask.redirect(flask.url_for("index"))
 
 @app.route("/sig")
 def sig():
+    if not logged_in(): return flask.redirect(flask.url_for("index"))
     return "sig sim"
+
+app.run()
