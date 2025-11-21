aa3f1f23de565991a0166f712f852f2dcf5c0831
Freddy <fredpashley1@gmail.com>
2025-11-21 20:04:52 +0000

    init

---
+++ app.py
@@ -0,0 +1,13 @@
+import flask
+import os
+
+os.chdir("./mysite/")
+app = flask.Flas(__name__)
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+app.route("/sig")
+def sig():
+    return "sig sim"
